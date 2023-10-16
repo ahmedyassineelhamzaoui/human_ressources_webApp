feec029871a941cfe639084d658a8900381c695d
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-16 11:44:13 +0100

    add a dashboard template

---
+++ src/main/java/com/app/controller/EmployeServlet.java
@@ -0,0 +1,35 @@
+package com.app.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.app.entity.Departement;
+import com.app.entity.Employe;
+
+
+
+public class EmployeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public EmployeServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("ok");
+	}
+
+}
--- src/main/java/com/app/entity/Employe.java
+++ src/main/java/com/app/entity/Employe.java
@@ -27,5 +27,93 @@ public class Employe {
     @OneToMany(mappedBy = "assignedEmployee")
     private List<Task> assignedTasks;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public List<Equipement> getAssignedEquipment() {
+		return assignedEquipment;
+	}
+
+	public void setAssignedEquipment(List<Equipement> assignedEquipment) {
+		this.assignedEquipment = assignedEquipment;
+	}
+
+	public Departement getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Departement department) {
+		this.department = department;
+	}
+
+	public List<Task> getAssignedTasks() {
+		return assignedTasks;
+	}
+
+	public void setAssignedTasks(List<Task> assignedTasks) {
+		this.assignedTasks = assignedTasks;
+	}
+
 
 }
