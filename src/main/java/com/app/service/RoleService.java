df3f2da4c54968d5f08d070349a91638c5f2c10a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-17 15:00:10 +0100

    create the method createRole method in roleService to implement the method saveRole in the RoleRepository

--- src/main/java/com/app/service/RoleService.java
+++ src/main/java/com/app/service/RoleService.java
@@ -1,5 +1,22 @@
 package com.app.service;
 
+import com.app.entity.Role;
+import com.app.repository.RoleRepository;
+import com.app.util.HibernateUtil;
+
+import jakarta.persistence.EntityManagerFactory;
+
 public class RoleService {
 
+	private final RoleRepository roleRepository;
+    private final EntityManagerFactory entityManagerFactory;
+
+    public RoleService() {
+        this.entityManagerFactory = HibernateUtil.getEntityManagerFactory();
+        this.roleRepository = new RoleRepository(entityManagerFactory);
+    }
+
+    public void createRole(Role role) {
+        roleRepository.saveRole(role);
+    }
 }
