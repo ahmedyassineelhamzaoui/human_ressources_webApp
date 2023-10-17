a2d33cc2bdeb90d62ac13984abe1928b9b31ee7c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-17 14:57:41 +0100

    create the method saveRole to create a role in database

--- src/main/java/com/app/repository/RoleRepository.java
+++ src/main/java/com/app/repository/RoleRepository.java
@@ -1,5 +1,31 @@
 package com.app.repository;
 
+import com.app.entity.Role;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+
 public class RoleRepository {
 
+	private final EntityManagerFactory entityManagerFactory;
+
+    public RoleRepository(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    public void saveRole(Role role) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist(role);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            e.printStackTrace(); 
+        } finally {
+            entityManager.close();
+        }
+    }
 }
