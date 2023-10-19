15b4dbcff444e393c0be6a5cd8aa7d8f2f88dfa0
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-19 11:49:38 +0100

    create the UserService

---
+++ src/main/java/com/app/service/UserService.java
@@ -0,0 +1,17 @@
+package com.app.service;
+
+import com.app.repository.UserRepository;
+import jakarta.persistence.*;
+import com.app.repository.UserRepository;
+import com.app.entity.User;
+public class UserService {
+
+	private final UserRepository userRepository;
+	
+	public UserService(EntityManager entityManager) {
+	    userRepository = new UserRepository(entityManager);
+	}
+	public void addUser(User user) {
+		userRepository.addUser(user);
+	}
+}
