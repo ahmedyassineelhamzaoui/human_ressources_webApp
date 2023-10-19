e88a0cff937733a9dc72e307fb646ebb28b16156
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-19 18:39:38 +0100

    change redirect path for the user

--- src/main/java/com/app/controller/LoginServlet.java
+++ src/main/java/com/app/controller/LoginServlet.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -43,7 +45,9 @@ public class LoginServlet extends HttpServlet {
 		User user = us.findByUserName(userLogin);
 		if(user !=null) {
 			if(BCrypt.checkpw(password, user.getPassword())) {
-				
+			   HttpSession session = request.getSession();
+			   session.setAttribute("user", user);
+			   response.sendRedirect("pages/tables/basic-table.jsp");
 			}else {
 				String errorMessage ="error="+URLEncoder.encode("invalid password",StandardCharsets.UTF_8);
 				response.sendRedirect("login.jsp?"+errorMessage);
