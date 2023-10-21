1573adb0ac6c508d588bc963a55782d745e8c87c
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-21 13:18:23 +0100

    implement the doGet method to get the user to edit with all information and roles,departments

--- src/main/java/com/app/controller/EditUserServlet.java
+++ src/main/java/com/app/controller/EditUserServlet.java
@@ -1,5 +1,6 @@
 package com.app.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,26 +8,55 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+import java.util.List;
+
+import com.app.entity.User;
+
+import com.app.entity.Departement;
+
+import com.app.entity.Role;
+
+import com.app.service.DepartmentService;
+import com.app.service.RoleService;
+import com.app.service.UserService;
+
 /**
  * Servlet implementation class EditUserServlet
  */
 public class EditUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public EditUserServlet() {
         super();
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String id = request.getParameter("id");
+		
+		UserService userService = new UserService();
+		
+		RoleService roleService = new RoleService();
+		
+		DepartmentService departmentService = new DepartmentService();
+		
+		List<Role> roles = roleService.getAllRoles();
+		
+		request.setAttribute("roles", roles);
+		
+		List<Departement> departments = departmentService.getAllDepartment();
+		
+		request.setAttribute("departments", departments);
+		
+		User user = userService.findUserById(Integer.parseInt(id));
+		
+		request.setAttribute("user", user);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("editUser.jsp");
+		
+		dispatcher.forward(request, response);
 	}
 
 	/**
