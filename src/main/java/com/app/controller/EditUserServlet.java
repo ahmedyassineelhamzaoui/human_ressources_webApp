9fc43d8892640d6da19e02f08efe2f4911cf8935
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-21 15:23:44 +0100

    add the userToEdit params in method updateUser in EditUserServlet

--- src/main/java/com/app/controller/EditUserServlet.java
+++ src/main/java/com/app/controller/EditUserServlet.java
@@ -97,7 +97,7 @@ public class EditUserServlet extends HttpServlet {
         }else {
             UserService userService = new UserService();
             
-
+            User usertoEdit = userService.findByUserLogin(userName);
             
             Role role = userService.findRoleById(Integer.parseInt(role_id)); 
             Departement department = userService.findDepartementById(Integer.parseInt(departement_id)); 
@@ -113,7 +113,7 @@ public class EditUserServlet extends HttpServlet {
             newUser.setPosition(position);
             newUser.setRole(role);
             newUser.setDepartment(department);
-            userService.updateUser(newUser);
+            userService.updateUser(newUser,usertoEdit.getId());
             String successMessage = "success="+ URLEncoder.encode("user updated successfuly",StandardCharsets.UTF_8);
         	response.sendRedirect("UserServlet?"+successMessage);
         }
