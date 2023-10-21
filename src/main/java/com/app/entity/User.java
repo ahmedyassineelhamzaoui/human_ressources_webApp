05f9fa5a498917f61dca173e5043a9fc4798392b
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-21 15:50:39 +0100

    add unique to the username and email colomuns

--- src/main/java/com/app/entity/User.java
+++ src/main/java/com/app/entity/User.java
@@ -11,10 +11,12 @@ public class User {
 	    @Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
+	    @Column(unique = true)
 	    private String username;
 	    private String password;
 	    private String firstName;
 	    private String lastName;
+	    @Column(unique = true)
 	    private String email;
 	    @Column(nullable=true)
 	    private String position;
