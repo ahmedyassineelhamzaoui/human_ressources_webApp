242963ef571964040fb33d94142ad7af2811517a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-21 15:08:05 +0100

    redirect user whene the user updated to users page

--- src/main/java/com/app/controller/EditUserServlet.java
+++ src/main/java/com/app/controller/EditUserServlet.java
@@ -7,9 +7,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+
+import org.mindrot.jbcrypt.BCrypt;
 
 import com.app.entity.User;
 
@@ -63,8 +65,58 @@ public class EditUserServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String userName = request.getParameter("userName");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String position = request.getParameter("position");
+        String role_id = request.getParameter("role");
+        String departement_id = request.getParameter("departement");
+        String emailRegex = "[^@ \\t\\r\\n]+@[^@ \\t\\r\\n]+\\.[^@ \\t\\r\\n]+";
+        
+        String err1="",err2="",err3="",err4="",err5="",err6="";
+        if(userName.trim().isEmpty()) {
+        	 err3 = "userNameError="+ URLEncoder.encode("username can't be empty", StandardCharsets.UTF_8);
+        }
+        if(firstName.trim().isEmpty()){
+        	 err4 = "firstNameError="+ URLEncoder.encode("first name can't be empty", StandardCharsets.UTF_8);
+        }
+        if(lastName.trim().isEmpty()){
+        	 err6 = "lastNameError="+ URLEncoder.encode("last name can't be empty", StandardCharsets.UTF_8);
+        }
+        if(email.trim().isEmpty() || !email.matches(emailRegex)){
+        	 err5 = "emailError="+ URLEncoder.encode("invalid email", StandardCharsets.UTF_8);
+        }
+        if(position.trim().isEmpty()){
+        	 err1 = "positionError="+ URLEncoder.encode("position can't be empty", StandardCharsets.UTF_8);
+        }
+       
+        if(!err1.isEmpty() || !err2.isEmpty() || !err3.isEmpty() || !err4.isEmpty() || !err5.isEmpty() || !err6.isEmpty()) {
+        	response.sendRedirect("pages/tables/editUser.jsp?"+err1+"&"+err2+"&"+err3+"&"+err4+"&"+err5+"&"+err6);
+        }else {
+            UserService userService = new UserService();
+            
+
+            
+            Role role = userService.findRoleById(Integer.parseInt(role_id)); 
+            Departement department = userService.findDepartementById(Integer.parseInt(departement_id)); 
+                
+                
+            User newUser = new User();
+            newUser.setUsername(userName);
+            String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+            newUser.setPassword(hashedPassword);
+            newUser.setFirstName(firstName);
+            newUser.setLastName(lastName);
+            newUser.setEmail(email);
+            newUser.setPosition(position);
+            newUser.setRole(role);
+            newUser.setDepartment(department);
+            userService.updateUser(newUser);
+            String successMessage = "success="+ URLEncoder.encode("user updated successfuly",StandardCharsets.UTF_8);
+        	response.sendRedirect("UserServlet?"+successMessage);
+        }
 	}
 
 }
