8ef1801f7da0771de154f5bfd0eb1ccbe41e7ed1
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-22 22:54:55 +0100

    create the constructor for the TaskRepository

--- src/main/java/com/app/repository/TaskRepository.java
+++ src/main/java/com/app/repository/TaskRepository.java
@@ -1,5 +1,14 @@
 package com.app.repository;
 
+import com.app.util.HibernateUtil;
+
+import jakarta.persistence.EntityManager;
+
 public class TaskRepository {
 
+	private EntityManager entityManager;
+	
+	public TaskRepository() {
+		this.entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+	}
 }
