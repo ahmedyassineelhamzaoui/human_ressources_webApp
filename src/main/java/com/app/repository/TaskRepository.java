f8e24177e984fb47c1c169124455e8ae0cb5ec4d
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-22 23:00:44 +0100

    create the add method for task

--- src/main/java/com/app/repository/TaskRepository.java
+++ src/main/java/com/app/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.app.repository;
 
+import com.app.entity.Task;
 import com.app.util.HibernateUtil;
 
 import jakarta.persistence.EntityManager;
@@ -10,5 +11,18 @@ public class TaskRepository {
 	
 	public TaskRepository() {
 		this.entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+	}
+	public void addTask(Task task) {
+		try {
+			entityManager.getTransaction().begin();
+			entityManager.persist(task);
+			entityManager.getTransaction().commit();
+		}catch(Exception e) {
+			if(entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().rollback();
+			}
+		}finally {
+			entityManager.close();
+		}
 	}
 }
