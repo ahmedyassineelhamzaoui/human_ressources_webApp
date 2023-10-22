850254d22b8f2e6ae1076557d22b58986c40178e
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-22 22:00:59 +0100

    create the method to add an equipment in EquipmentRepository

--- src/main/java/com/app/repository/EquipmentRepository.java
+++ src/main/java/com/app/repository/EquipmentRepository.java
@@ -1,5 +1,27 @@
 package com.app.repository;
 
+import com.app.entity.Equipement;
+import com.app.util.HibernateUtil;
+
+import jakarta.persistence.EntityManager;
+
 public class EquipmentRepository {
 
+	private EntityManager entityManager;
+	public EquipmentRepository() {
+		entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+	}
+	public void AddEquipment(Equipement equipment) {
+		try {
+		     entityManager.getTransaction().begin();
+		     entityManager.persist(equipment);
+		     entityManager.getTransaction().commit();
+		}catch(Exception e) {
+			if(entityManager.getTransaction().isActive()) {
+				entityManager.getTransaction().rollback();
+			}
+		}finally {
+			entityManager.close();
+		}
+	}
 }
