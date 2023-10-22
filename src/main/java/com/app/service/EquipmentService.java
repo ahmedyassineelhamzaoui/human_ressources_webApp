6095d06476f145934bcd9912474a4e9e3b384854
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-22 22:21:03 +0100

    create the saveEquipment method

--- src/main/java/com/app/service/EquipmentService.java
+++ src/main/java/com/app/service/EquipmentService.java
@@ -1,5 +1,6 @@
 package com.app.service;
 
+import com.app.entity.Equipement;
 import com.app.repository.EquipmentRepository;
 
 public class EquipmentService {
@@ -8,6 +9,9 @@ public class EquipmentService {
 	
 	public EquipmentService() {
 		this.equipementRepostiry = new EquipmentRepository();
+	}
+	public void SaveEquipment(Equipement equipement) {
+		equipementRepostiry.AddEquipment(equipement);
 	}
 	
 }
