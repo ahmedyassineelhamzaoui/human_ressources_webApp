868a68f2b5b97bd4acd7f6c06b97acf5ac16d459
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-23 15:01:14 +0100

    update the login method in servlet

--- src/main/java/com/app/controller/LoginServlet.java
+++ src/main/java/com/app/controller/LoginServlet.java
@@ -44,7 +44,7 @@ public class LoginServlet extends HttpServlet {
 			if(BCrypt.checkpw(password, user.getPassword())) {
 			   HttpSession session = request.getSession();
 			   session.setAttribute("user", user);
-			   response.sendRedirect("build/pages/UserServlet");
+			   response.sendRedirect("pages/tables/UserServlet");
 			}else {
 				String errorMessage ="error="+URLEncoder.encode("invalid password",StandardCharsets.UTF_8);
 				response.sendRedirect("login.jsp?"+errorMessage);
