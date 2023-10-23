f361733e32f8ae5c9b1e9e457e6d50c9cae25c54
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-23 10:51:18 +0100

    store equipment in database using EquipmentServlet

--- src/main/java/com/app/controller/EquipmentServlet.java
+++ src/main/java/com/app/controller/EquipmentServlet.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.app.entity.Equipement;
 import com.app.entity.User;
@@ -42,18 +44,30 @@ public class EquipmentServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String name =request.getParameter("name");
-		String purchaseDate =request.getParameter("purchaseDate");
-		String maintenaceDate =request.getParameter("maintenaceDate");
+		String purchaseDateStr =request.getParameter("purchaseDate");
+		String maintenaceDateStr =request.getParameter("maintenaceDate");
 		String status =request.getParameter("status");
 		String type =request.getParameter("type");
 		String user_id =request.getParameter("user_id");
-		PrintWriter pw = response.getWriter();
 		UserService  userService = new UserService();
 		User user = userService.findUserById(Integer.parseInt(user_id));
 		EquipmentService equipmentService = new EquipmentService();
 		Equipement e = new Equipement();
-	
-		pw.println("name : "+name+" purchaseDate: "+purchaseDate+" status: "+status+" type: "+type+" user_id: "+user_id+" maintenaceDate"+maintenaceDate);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date purchaseDate = dateFormat.parse(purchaseDateStr);
+		    Date maintenanceDate = dateFormat.parse(maintenaceDateStr);
+		    e.setMaintenanceDate(maintenanceDate);
+		    e.setPurchaseDate(purchaseDate);
+		}catch(Exception es) {
+			es.getMessage();
+	    }
+		
+	    e.setName(name);
+	    e.setStatus(status);
+	    e.setType(type);
+	    e.setAssignedUser(user);
+	    equipmentService.SaveEquipment(e);
 	}
 
 }
