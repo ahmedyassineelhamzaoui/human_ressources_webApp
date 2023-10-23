44f52ce1f2e44a38c7ebcf0989c54c939ee76051
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-23 10:28:06 +0100

    create the doGet method to display the equipment.jsp file

--- src/main/java/com/app/controller/EquipmentServlet.java
+++ src/main/java/com/app/controller/EquipmentServlet.java
@@ -1,5 +1,6 @@
 package com.app.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -26,14 +27,15 @@ public class EquipmentServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("equipment.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
