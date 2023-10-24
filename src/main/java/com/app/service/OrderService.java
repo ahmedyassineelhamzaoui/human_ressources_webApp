3af677019fad59af1e5763de412cd84e5ad8b884
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-24 11:01:12 +0100

    creeate constructor of OrderService

--- src/main/java/com/app/service/OrderService.java
+++ src/main/java/com/app/service/OrderService.java
@@ -1,5 +1,11 @@
 package com.app.service;
 
+import com.app.repository.OrderRepository;
+
 public class OrderService {
 
+	private OrderRepository orderRepository;
+	public OrderService() {
+		this.orderRepository = new OrderRepository();
+	}
 }
