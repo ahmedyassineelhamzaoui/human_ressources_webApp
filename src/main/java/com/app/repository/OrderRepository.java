edf644eee07a4898a00739c20b97b4b02dbbb938
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-24 10:48:15 +0100

    create constructor of OrderRepository

--- src/main/java/com/app/repository/OrderRepository.java
+++ src/main/java/com/app/repository/OrderRepository.java
@@ -1,5 +1,12 @@
 package com.app.repository;
 
+import com.app.util.HibernateUtil;
+
+import jakarta.persistence.EntityManager;
 public class OrderRepository {
 
+	private EntityManager entityManager;
+	public OrderRepository() {
+		this.entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+	}
 }
