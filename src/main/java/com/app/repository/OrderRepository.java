6f138b4ad3ecd0d21b461b111c80dc473cfeaa36
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-24 10:51:01 +0100

    create addOrder method

--- src/main/java/com/app/repository/OrderRepository.java
+++ src/main/java/com/app/repository/OrderRepository.java
@@ -1,12 +1,27 @@
 package com.app.repository;
 
 import com.app.util.HibernateUtil;
-
+import com.app.entity.Order;
 import jakarta.persistence.EntityManager;
 public class OrderRepository {
 
 	private EntityManager entityManager;
 	public OrderRepository() {
 		this.entityManager = HibernateUtil.getEntityManagerFactory().createEntityManager();
+	}
+	public void addOrder(Order order) {
+		try {
+			entityManager.getTransaction().begin();
+			entityManager.persist(order);
+			entityManager.getTransaction().commit();
+			}catch(Exception e) {
+				if(entityManager.getTransaction().isActive()) {
+					entityManager.getTransaction().rollback();
+				}
+			e.getMessage();
+		}finally {
+			entityManager.close();
+		}
+		
 	}
 }
