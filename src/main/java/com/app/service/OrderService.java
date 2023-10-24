d1eac697b05e3161d041d9e62ba920c54df40e30
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-24 11:02:19 +0100

    create the addOrder method in OrderService

--- src/main/java/com/app/service/OrderService.java
+++ src/main/java/com/app/service/OrderService.java
@@ -1,11 +1,14 @@
 package com.app.service;
 
 import com.app.repository.OrderRepository;
-
+import com.app.entity.Order;
 public class OrderService {
 
 	private OrderRepository orderRepository;
 	public OrderService() {
 		this.orderRepository = new OrderRepository();
+	}
+	public void addOrder(Order order) {
+		orderRepository.addOrder(order);
 	}
 }
