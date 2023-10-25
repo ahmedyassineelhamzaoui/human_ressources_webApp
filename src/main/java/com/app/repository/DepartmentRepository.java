d382aafcdcdc8c4995ce0a2faf01eb32c3cb7ac4
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-25 18:50:35 +0100

    remove the finally block for addDepartment method

--- src/main/java/com/app/repository/DepartmentRepository.java
+++ src/main/java/com/app/repository/DepartmentRepository.java
@@ -24,8 +24,6 @@ public class DepartmentRepository {
 				entityManager.getTransaction().rollback();
 			}
 			e.getMessage();
-		}finally {
-			entityManager.close();
 		}
 	}
 	public List<Departement> getAllDepartments(){
