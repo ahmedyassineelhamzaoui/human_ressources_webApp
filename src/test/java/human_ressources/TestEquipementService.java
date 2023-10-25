50afe695b941fffb362eb6a514ebb0f6c4dd3899
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-25 19:28:18 +0100

    create the test method to add an equipment

--- src/test/java/human_ressources/TestEquipementService.java
+++ src/test/java/human_ressources/TestEquipementService.java
@@ -6,24 +6,40 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.app.entity.Equipement;
 import com.app.service.EquipmentService;
-
+import com.app.service.UserService;
+import com.app.entity.*;
 public class TestEquipementService {
 
 	private EquipmentService equipmentService;
+	private UserService userService;
 	@Before
 	public void setUp() throws Exception {
 		equipmentService = new EquipmentService();
+		userService      = new UserService();
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		equipmentService = null;
+		userService      = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddEquipment() {
+	    Equipement equipment = new Equipement();
+	    equipment.setName("equipment name");
+	    equipment.setStatus("valable");
+	    equipment.setMaintenanceDate(null);
+	    equipment.setPurchaseDate(null);
+	    equipment.setType("type");
+	    User user = userService.findUserById(1);
+	    equipment.setAssignedUser(user);
+	    equipmentService.SaveEquipment(equipment);
+	    Equipement equipmentFounded = equipmentService.findById(equipment.getId());
+	    assertNotNull(equipmentFounded);
+	    equipmentService.deleteEquipment(equipmentFounded);
 	}
 
 }
