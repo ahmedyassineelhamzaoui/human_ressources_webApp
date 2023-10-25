7a4521cf2fdba911150f22c0a5c32bde5e4d3814
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-25 19:49:11 +0100

    create the test method for delete Equipment

--- src/test/java/human_ressources/TestEquipementService.java
+++ src/test/java/human_ressources/TestEquipementService.java
@@ -6,7 +6,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.app.entity.Equipement;
 import com.app.service.EquipmentService;
 import com.app.service.UserService;
 import com.app.entity.*;
@@ -41,5 +40,22 @@ public class TestEquipementService {
 	    assertNotNull(equipmentFounded);
 	    equipmentService.deleteEquipment(equipmentFounded);
 	}
+	 @Test
+     public void testRemoveEquipment() {
+ 	    Equipement equipment = new Equipement();
+ 	    equipment.setName("equipment name");
+	    equipment.setStatus("valable");
+	    equipment.setMaintenanceDate(null);
+	    equipment.setPurchaseDate(null);
+	    equipment.setType("type");
+	    User user = userService.findUserById(1);
+	    equipment.setAssignedUser(user);
+	    equipmentService.SaveEquipment(equipment);
 
+	    Equipement equipmentFounded = equipmentService.findById(equipment.getId());
+        assertNotNull(equipmentFounded);
+        equipmentService.deleteEquipment(equipmentFounded);
+        Equipement equipmentDeleted = equipmentService.findById(equipmentFounded.getId());
+        assertNull(equipmentDeleted);
+     }
 }
