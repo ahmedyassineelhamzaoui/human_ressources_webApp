ab5449eaca35048a624508a3b5de623186091a78
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-26 09:40:38 +0100

    create a method to display swit alert when user try to delete equipment

--- src/main/webapp/pages/assets/script.js
+++ src/main/webapp/pages/assets/script.js
@@ -167,3 +167,18 @@ function deleteRoleData(roleId){
       	    }
 	  });
 }
+function showDeleteConfirmationEquipment(equipmentId){
+	Swal.fire({
+		title: 'delete equipment',
+		text: 'Are you sure you want to delete this equipment ?',
+		icon:'warning',
+		showCancelButton:true,
+		confirmButtonText:'Yes,delete it',
+		cancelButtonText:'No cancel',
+		confirmButtonColor:'#d33',
+	}).then((result)=>{
+		if(result.isConfirmed){
+			deleteEquipmentData(equipmentId);
+		}
+	})
+}
