e6ff5917c944839ce7e6be4383b0208305a12304
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-26 09:15:38 +0100

    create the getters and setters for users in role

--- src/main/java/com/app/entity/Role.java
+++ src/main/java/com/app/entity/Role.java
@@ -20,6 +20,7 @@ public class Role {
 	private Long id;
 	private String name;
 	
+	
 	@OneToMany(mappedBy = "role")
     private List<User> users;
 	
@@ -35,5 +36,10 @@ public class Role {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+	public List<User> getUsers() {
+		return users;
+	}
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
 }
--- src/main/java/com/app/entity/User.java
+++ src/main/java/com/app/entity/User.java
@@ -151,5 +151,6 @@ public class User {
 	            department = null;
 	        }
 	    }
+		
 
 }
