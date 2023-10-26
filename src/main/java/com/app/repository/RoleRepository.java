37a8df6005e0d3857fdffd03dd3ae96bb3e87159
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2023-10-26 15:40:27 +0100

    cretae the update role method in roleRepository

--- src/main/java/com/app/repository/RoleRepository.java
+++ src/main/java/com/app/repository/RoleRepository.java
@@ -3,6 +3,7 @@ package com.app.repository;
 import java.util.List;
 
 import com.app.entity.Role;
+import com.app.entity.User;
 import com.app.util.HibernateUtil;
 
 import jakarta.persistence.EntityManager;
@@ -41,5 +42,19 @@ public class RoleRepository {
     }
     public Role findById(long id) {
         return entityManager.find(Role.class, id); 
+    }
+    public void updateRole(Role role,long id) {
+    	Role roleToEdit = entityManager.find(Role.class, id);
+		try {
+			entityManager.getTransaction().begin();
+			roleToEdit.setName(role.getName());
+			entityManager.getTransaction().commit();
+		}catch(Exception e) {
+			if(entityManager.getTransaction().isActive()) 
+			{
+				entityManager.getTransaction().rollback();
+			}
+			e.getMessage();
+		}
     }
 }
